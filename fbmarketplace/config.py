d07778c4b9b3b3cf7d6089ec52504a737462dd80
Matt Gropper <mgropps17@gmail.com>
2020-11-12 19:16:10 -0500

    fixed connection issue between DB and flask

--- fbmarketplace/config.py
+++ fbmarketplace/config.py
@@ -15,7 +15,7 @@ SESSION_COOKIE_NAME = 'login'
 # File Upload to var/uploads/
 UPLOAD_FOLDER = os.path.join(
     os.path.dirname(os.path.dirname(os.path.realpath(__file__))),
-    'db', 'uploads'
+    'var', 'uploads'
 )
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 MAX_CONTENT_LENGTH = 16 * 1024 * 1024
@@ -23,5 +23,5 @@ MAX_CONTENT_LENGTH = 16 * 1024 * 1024
 # Database file is var/fbmarketplace.sqlite3
 DATABASE_FILENAME = os.path.join(
     os.path.dirname(os.path.dirname(os.path.realpath(__file__))),
-    'db', 'fbmarketplace.sqlite3'
+    'var', 'fbmarketplace.sqlite3'
 )
