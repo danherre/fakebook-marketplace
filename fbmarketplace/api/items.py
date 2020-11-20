53f11a5a34d6ad55b603a63ac1f0fd808c09ffc3
amanzhan <amanzhan@umich.edu>
2020-11-20 12:38:26 -0500

    Wrote APIs for user items and user reivews

--- fbmarketplace/api/items.py
+++ fbmarketplace/api/items.py
@@ -1,5 +1,8 @@
 import flask
 from flask import session, abort
+import tempfile
+import shutil
+import os
 import fbmarketplace
 from fbmarketplace.api.hashing import sha256sum
 
@@ -16,6 +19,15 @@ def get_item_data(line):
     response['available'] = bool(line['available'])
     return response
 
+def get_review_data(line):
+    """Translate the database output for a post into a dictionary."""
+    response = {}
+    response['reviewid'] = int(line['reviewid'])
+    response['writer'] = line['writer']
+    response['writee'] = line['writee']
+    response['review'] = line['review']
+    response['created'] = line['created']
+    return response 
 
 @fbmarketplace.app.route('/api/items/', methods=['GET'])
 def get_items():
@@ -46,7 +58,7 @@ def get_items():
 
 
 @fbmarketplace.app.route('/api/items/<username>/', methods=['GET', 'POST'])
-def get_items_by_user(username):
+def items_by_user(username):
     connection = fbmarketplace.model.get_db()
     response = {}
     if flask.request.method=='GET':
@@ -60,10 +72,57 @@ def get_items_by_user(username):
             page = 0
         response['next'] = '/api/items/' + username + '/?page=' + str(page+1)
     else: # TODO: Figure out how to save file
-        if 'image' in flask.request.files:
-            image = flask.request.files['image']
-            filename = sha256sum(image.filename)
-            image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        flask.request.form['description']
+        if "username" not in session:
+            abort(403)
+        dummy, temp_filename = tempfile.mkstemp()
+        image = flask.request.files['image']
+        image.save(temp_filename)
+        hash_txt = sha256sum(temp_filename)
+        dummy, suffix = os.path.splitext(image.filename)
+        hash_filename_basename = hash_txt + suffix
+        hash_filename = os.path.join(
+            fbmarketplace.app.config["UPLOAD_FOLDER"],
+            hash_filename_basename
+        )
+        shutil.move(temp_filename, hash_filename)
+        cur = connection.execute(
+            "INSERT INTO items (owner, name, description, price, available, image) VALUES ('" + 
+            username + "', " + 
+            flask.request.form['name'] + "', '" + 
+            flask.request.form['description'] + "', '" + 
+            flask.request.form['price'] + "', '" + 
+            flask.request.form['available'] + "', '" + 
+            hash_filename_basename + "')"
+        )
+        response['Success'] = True
 
     return flask.jsonify(**response)
+
+
+@fbmarketplace.app.route('/api/reviews/<username>/', methods=['GET', 'POST'])
+def reviews_by_user(username):
+    connection = fbmarketplace.model.get_db()
+    response = {}
+    # if GET request, return 4 newest reviews of user
+    if flask.request.method=='GET':
+        page = flask.request.args.get("page", default=0, type=int)
+        cur = connection.execute(
+            "SELECT * FROM reviews WHERE writee='" + username + "' ORDER BY reviewid DESC LIMIT 4 OFFSET " + str(4*page)
+        )
+        items = cur.fetchall()
+        response['results'] = [get_review_data(item) for item in items]
+        if flask.request.args.get("page") is None:
+            page = 0
+        response['next'] = '/api/reviews/' + username + '/?page=' + str(page+1)
+    else: 
+        if "username" not in session:
+            abort(403)
+        cur = connection.execute(
+            "INSERT INTO reviews (writer, writee, review, rating) VALUES ('" + 
+            flask.session['username'] + "', '" + 
+            username + "', '" + 
+            flask.session['review'] + "', '" + 
+            flask.session['rating'] + "')"
+        )
+        response['Success'] = True
+    return flask.jsonify(**response)    
